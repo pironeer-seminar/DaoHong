38ac7d681a069c1a6ba86398d6c78f68392af4de
Dao Hong <mmtsd3333@gmail.com>
2025-03-21 15:50:48 +0900

    week 2 과제 완료

---
+++ W2_Assignment/src/main/java/com/example/sentenceanalysis/dto/SentenceRequest.java
@@ -0,0 +1,13 @@
+package com.example.sentenceanalysis.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SentenceRequest {
+
+    @NotBlank(message = "문장은 필수 입력 값입니다.")
+    private String sentence;
+}
